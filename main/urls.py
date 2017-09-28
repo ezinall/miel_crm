d280f68fdb9f4d68787afddfa58ef94d8e00d6d2
Ruslan <vasilevskiy.r@gmail.com>
2017-09-28 19:53:55 +0300

    main html

--- main/urls.py
+++ main/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
+from django.views.generic import TemplateView
 
-admin.autodiscover()
+app_name = 'main'
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('main.urls')),
+    url(r'^$', TemplateView.as_view(template_name='main/index.html'), name='index'),
 ]
